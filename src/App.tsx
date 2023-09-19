705cbc018ecafd06148230850efda4ff845f5579
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-09-19 04:17:23 +0000

    Update 8 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom"; // react-router-dom 설치(일반/type 두번) -> 
 import "./styles.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
