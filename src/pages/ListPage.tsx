f8246c9880500d7aceb7da9ac7e2078f7346117a
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-09-20 06:05:51 +0000

    Update 3 files

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-import "./../styles.css";
+import List from "../components/List/List";
 
 export default function ListPage() {
-  return <div>List</div>;
+  const dataList = [
+    {
+      id: "listId01",
+      name: "김철수",
+      date: "23.01.01",
+      text: "긴글 입력하는 곳",
+      label: ["고위험", "중위험"],
+    },
+    {
+      id: "listId02",
+      name: "김영희",
+      date: "23.01.01",
+      text: "긴글 입력하는 곳",
+      label: ["저위험"],
+    },
+    { id: "listId03", name: "매머드", date: "23.01.01" },
+  ];
+
+  return <List dataList={dataList} />;
 }
