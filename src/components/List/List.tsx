245342412b9b40d446c7a6cae39b7d3ad5492535
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-09-20 07:41:12 +0000

    Update 2 files

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ListItem from "./ListItem";
 
-export default function List() {
+export default function List(props) {
+  const $li = props.dataList;
+  const dataLists = $li.map((data) => (
+    <ListItem key={data.id} {...data} />
+  ));
   return (
-    <div>List ddd</div>
+    <>
+      <ul className="list_bul">{dataLists}</ul>
+    </>
   );
 }
---
+++ src/components/List/ListItem.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function ListItem(props) {
+  const lableBox = props.label;
+  //데이터 여부 체크
+  const labelSize = props.label == undefined ? false : true;
+  const nameSize = props.name == undefined ? false : true;
+  const textSize = props.text == undefined ? false : true;
+
+  const labelList =
+    labelSize === true && //label 없을 때
+    lableBox.map((label) => {
+      let $class;
+
+      switch (label) {
+        case "고위험":
+          $class = "red";
+          break;
+        case "중위험":
+          $class = "orange";
+          break;
+        case "저위험":
+          $class = "green";
+          break;
+        default:
+          $class = "";
+      }
+
+      return (
+        <div key={$class} className={$class}>
+          {label}
+        </div>
+      );
+    });
+
+  return (
+    <li key={props.id}>
+      {labelSize ? <div className="wrap_label">{labelList}</div> : null}
+      {nameSize ? <div>{props.name},</div> : null}
+      {textSize ? <div>{props.text},</div> : null}
+      {props.date}
+    </li>
+  );
+}
+
+export default ListItem;
